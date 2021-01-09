ea3517dbbab896aef6e85bd8fb7589e2e927dd3a
Scott Mudge <19617165+scottmudge@users.noreply.github.com>
2021-01-09 00:19:16 -0500

    Update song_player.py
    Fix note length

--- OctoPrint_MeatPack/song_player.py
+++ OctoPrint_MeatPack/song_player.py
@@ -20,7 +20,7 @@ def get_note_freq(note: str, octave: int) -> int:
 
 
 BPM = 140
-EightNote = int(round(((60.0 / BPM) / 8.0)))
+EightNote = int(round(((60.0 / BPM) / 8.0))) * 1000
 QuarterNote = int(round(EightNote * 2.0))
 HalfNote = int(round(QuarterNote * 2.0))
 FullNote = int(round(HalfNote * 2.0))
