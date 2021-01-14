2102b2bfbf4155197e54f1472c0c7430d55eea4f
Scott Mudge <mail@scottmudge.com>
2021-01-14 18:06:22 -0500

    forgot to update version for pip update.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ plugin_package = "OctoPrint_MeatPack"
 plugin_name = "OctoPrint-MeatPack"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.3.1"
+plugin_version = "1.3.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
